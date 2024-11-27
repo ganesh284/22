4239fcf9872a1ca03b3398551b93ed5ad910029f
ganesh284 <ganeshpepeti814@gmail.com>
2024-11-27 22:10:21 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./Home";
+import First from "./First"; // Import First component
+import Second from "./Second";
+import Third from "./Third";
+import Forth from "./Forth";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/first" element={<First />} /> {/* Define First route */}
+        <Route path="/second" element={<Second />} />
+        <Route path="/third" element={<Third />} />
+        <Route path="/forth" element={<Forth />} />
+      </Routes>
+    </Router>
   );
 }
 
